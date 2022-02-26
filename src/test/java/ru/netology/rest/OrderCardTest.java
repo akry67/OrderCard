9b75c1637b58f896d2ca7d7413f270af6dcac748
Anna Kuznetsova <akry67@gmail.com>
2022-02-26 22:42:33 +0400

    css.selectors

--- src/test/java/ru/netology/rest/OrderCardTest.java
+++ src/test/java/ru/netology/rest/OrderCardTest.java
@@ -49,11 +49,11 @@ class ChromeTest {
         // Your test logic here
         driver.get("http://localhost:9999");
         List<WebElement> elements = driver.findElements(By.className("input__control"));
-        elements.get(0).sendKeys("Анна");
-        elements.get(1).sendKeys("+79270000000");
-        driver.findElement(By.className("checkbox__box")).click();
+        driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Анна Кузнецова-Макалова");
+        driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+79270000000");
+        driver.findElement(By.cssSelector("[data-test-id=agreement]")).click();
         driver.findElement(By.tagName("button")).click();
-        String text = driver.findElement(By.className("paragraph")).getText();
+        String text = driver.findElement(By.cssSelector("[data-test-id =order-success]")).getText();
         assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text.trim());
 
     }
